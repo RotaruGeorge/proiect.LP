aa31a261831c215fa19e04a52337f258cab1e1aa
RotaruGeorge <83924463+RotaruGeorge@users.noreply.github.com>
2021-05-17 01:57:48 +0300

    Add files via upload

--- proiect.py
+++ proiect.py
@@ -5,6 +5,7 @@ from tkinter import filedialog
 import os
 import tkinter as tk
 from PIL import Image, ImageTk
+
 def showimage():
     fln = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select image file", filetypes=(("JPG File", "*.jpg"), ("PNG file", "*.png"), ("All File", "*.*")))
     img = Image.open(fln)
@@ -12,6 +13,40 @@ def showimage():
     img = ImageTk.PhotoImage(img)
     lbl.configure(image=img)
     lbl.image = img
+    img=cv2.imread(fln)
+
+    image_Gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    window_name='imagine Gray'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name,image_Gray)
+
+    image_XYZ = cv2.cvtColor(img, cv2.COLOR_RGB2XYZ)
+    window_name='Imagine XYZ'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name,image_XYZ)
+
+    image_LAB = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    window_name='Imagine_LAB'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name,image_LAB)
+
+    image_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    window_name='Imagine_BGR'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name,image_BGR)
+
+    image_HSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    window_name='Imagine_HSV'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name,image_HSV)
+
+    image_HLS = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    window_name='Imagine_HLS'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.imshow(window_name,image_HLS)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 root = Tk()
 
@@ -21,36 +56,15 @@ frm.pack(side=BOTTOM, padx=15, pady=15)
 lbl= Label(root)
 lbl.pack()
 
-btn = Button(frm, text="browser image",command=showimage)
+btn = Button(frm, text="Cauta imagine",command=showimage)
 btn.pack(side=tk.LEFT)
 
 btn = Button(frm, text="Exit",command=lambda: exit())
 btn.pack(side=tk.LEFT, padx=10)
 
-root.title("image Browser")
+
+
+root.title("Imagine RGB")
 root.geometry("300x500")
 root.mainloop()
-
-img=cv2.imread("image.jpg")
-window_name='imagine RGB'
-cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.imshow(window_name,img)
-
-image_Gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-window_name='imagine Gray'
-cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.imshow(window_name,image_Gray)
-
-image_XYZ = cv2.cvtColor(img, cv2.COLOR_RGB2XYZ)
-window_name='Imagine XYZ'
-cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.imshow(window_name,image_XYZ)
-
-image_HSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-window_name='Imagine_HSV'
-cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-cv2.imshow(window_name,image_HSV)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
